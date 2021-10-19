48e8d3eec7a31b0f020b256c2ebaa7c84407b908
dan1aa <burmeidan@gmail.com>
2021-10-19 15:52:50 +0300

    try/catch:

--- index.js
+++ index.js
@@ -10,23 +10,28 @@ let server = http.createServer((req, res) => {
     res.end("Home page");
   } 
   else if (req.method === "GET" && req.url.includes("/download")) {
-    let currentURL = new URL(`http://localhost:3000${req.url}`);
-    let fileURL = currentURL.searchParams.get("url");
-    let fileName = fileURL.split("/").slice(-1).toString();
-    let fileWriteStream = fs.createWriteStream(directory + "\\" + fileName)
-    let zipWriteStream = fs.createWriteStream(directory + "\\" + `${fileName}.zip`)
-    const archive = archiver('zip', {
-      zlib: {
-        level: 9
-      }
-    })
-    let request = https.get(fileURL, (img) => {
-      img.pipe(fileWriteStream)
-      archive.pipe(zipWriteStream)
-      archive.append(fs.createReadStream(directory + "\\" + fileName), {name: fileName})
-      archive.finalize()
-    })
-    res.end()
+    try {
+      let currentURL = new URL(`http://localhost:3000${req.url}`);
+      let fileURL = currentURL.searchParams.get("url");
+      let fileName = fileURL.split("/").slice(-1).toString();
+      let fileWriteStream = fs.createWriteStream(directory + "\\" + fileName)
+      let zipWriteStream = fs.createWriteStream(directory + "\\" + `${fileName}.zip`)
+      const archive = archiver('zip', {
+        zlib: {
+          level: 9
+        }
+      })
+      let request = https.get(fileURL, (img) => {
+        img.pipe(fileWriteStream)
+        archive.pipe(zipWriteStream)
+        archive.append(fs.createReadStream(directory + "\\" + fileName), {name: fileName})
+        archive.finalize()
+      })
+      res.end()
+    }
+    catch(e) {
+      throw new Error(e)
+    }
   }
 })
 
