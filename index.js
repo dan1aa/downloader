dad1e4795011fd80093e977f76cd26baa5dcd35c
dan1aa <burmeidan@gmail.com>
2021-10-19 15:48:09 +0300

    homework

--- index.js
+++ index.js
@@ -1,28 +1,35 @@
-const exOne = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
-const exTwo = '/download';
-const regex = new RegExp("^" + exTwo + exOne + "$", 'i');
-const http = require('http')
-const download = require('file-download')
-
-const fileUrl = 'https://freesoft.ru/storage/images/news/1/6/555/555_text.png';
-
-
+const http = require("http");
+const path = require('path')
+const fs = require('fs')
+const archiver = require('archiver')
+const https = require('https')
+const directory = `${process.env.HOME}\\\Downloads`;
 
 let server = http.createServer((req, res) => {
-    if(req.method === 'GET' && req.url.match(regex)) {
-        var options = {
-            directory: "./",
-            filename: "test.png"
-        }
-        
-        download(fileUrl, options, function(err){
-            if (err) throw err
-            console.log("success")
-        }) 
-        res.end()
-    }
+  if (req.method === "GET" && req.url === "/") {
+    res.end("Home page");
+  } 
+  else if (req.method === "GET" && req.url.includes("/download")) {
+    let currentURL = new URL(`http://localhost:3000${req.url}`);
+    let fileURL = currentURL.searchParams.get("url");
+    let fileName = fileURL.split("/").slice(-1).toString();
+    let fileWriteStream = fs.createWriteStream(directory + "\\" + fileName)
+    let zipWriteStream = fs.createWriteStream(directory + "\\" + `${fileName}.zip`)
+    const archive = archiver('zip', {
+      zlib: {
+        level: 9
+      }
+    })
+    let request = https.get(fileURL, (img) => {
+      img.pipe(fileWriteStream)
+      archive.pipe(zipWriteStream)
+      archive.append(fs.createReadStream(directory + "\\" + fileName), {name: fileName})
+      archive.finalize()
+    })
+    res.end()
+  }
 })
 
 server.listen(3000, () => {
-    console.log('server on 3000');
+    console.log('listened');
 })
